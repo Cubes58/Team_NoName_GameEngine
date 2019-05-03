1808d5dbd7fd499366859b8535bf74195899ac41
Aadi <P16198363@GWHXX632WIN003W>
2019-05-03 12:01:06 +0100

    Added ifdef and endif for debug mode

--- GameEngineArchitecture/source/RenderEngine.cpp
+++ GameEngineArchitecture/source/RenderEngine.cpp
@@ -22,6 +22,7 @@
 #include "EnemyTower.h"
 #include "EndLevelCollectable.h"
 
+#define DEBUG
 
 void RenderEngine::Init(int p_ScreenWidth, int p_ScreenHeight)
 {
@@ -111,26 +112,30 @@ void RenderEngine::Render()
 	m_ShadowShader->SetInt("depthMap", m_ShadowRenderer->GetShadowMap());
 	RenderSceneObjects(m_ShadowShader);
 
+	m_Skybox->Render();
 
+	// debug
+#ifdef USE_RENDER_DEBUG_IMGUI
 	m_DefaultShader->Use();
 	m_DefaultShader->ErrorChecker();
 	m_DefaultShader->SetMat4("model", m_Sun->GetModelMatrix());
 	SetShaderParams(m_DefaultShader);
 	SetLightParams(m_DefaultShader);
 	m_Sun->Render();
-	
 
-	m_Skybox->Render();
 	ImGuiRender();
-	//RenderDebugging();
+#endif
 	
 }
 
+//#ifdef DEBUG
 void RenderEngine::ImGuiRender()
 {
+	//bool window_open = true;
 	static bool show_test_window = true;
 	static bool show_another_window = false;
 	ImVec4 clear_color = ImColor(114, 144, 154);
+
 
 	/////////////////////////////////////////////////////////////
 	//move to scene??
@@ -153,15 +158,15 @@ void RenderEngine::ImGuiRender()
 		//loading 
 
 	//	//static float f = 0.0f;
-	ImGui::Text("Hello, world!");
-	ImGui::SliderFloat("X axis", fpx, 0.0f, 2.0f);
-	ImGui::SliderFloat("Y axis", fpy, 0.0f, 2.0f);
-	ImGui::SliderFloat("Z axis", fpz, 0.0f, 2.0f);
-	ImGui::ColorEdit3("clear color", (float*)&clear_color);
-	if (ImGui::Button("Add Object")) MODEL_MANAGER.GetModel("longWall");
-	if (ImGui::Button("Test Window")) show_test_window ^= 1;
-	//if (ImGui::Button("Another Window")) show_another_window ^= 1;
-	ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+		ImGui::Text("Hello, world!");
+		ImGui::SliderFloat("X axis", fpx, 0.0f, 2.0f);
+		ImGui::SliderFloat("Y axis", fpy, 0.0f, 2.0f);
+		ImGui::SliderFloat("Z axis", fpz, 0.0f, 2.0f);
+		ImGui::ColorEdit3("clear color", (float*)&clear_color);
+		if (ImGui::Button("Add Object")) MODEL_MANAGER.GetModel("longWall");
+		if (ImGui::Button("Test Window")) show_test_window ^= 1;
+		//if (ImGui::Button("Another Window")) show_another_window ^= 1;
+		ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
 	}
 
 	// 2. Show another simple window, this time using an explicit Begin/End pair
@@ -187,7 +192,10 @@ void RenderEngine::ImGuiRender()
 	//glClearColor(clear_color.x, clear_color.y, clear_color.z, clear_color.w);
 	//glClear(GL_COLOR_BUFFER_BIT);
 	ImGui::Render();
+
 }
+//#endif DEBUG
+
 
 void RenderEngine::RenderFrameBuffers()
 {
@@ -218,6 +226,7 @@ void RenderEngine::RenderSceneObjects(std::shared_ptr<ShaderProgram> p_ShaderPro
 
 		if (modelComponent != nullptr && transformComponent != nullptr)
 			DrawModel(modelComponent->GetModel(), transformComponent->GetModelMatrix(), p_ShaderProgram);
+#ifdef USE_RENDER_DEBUG_IMGUI
 		if (aabbBoxComponent != nullptr && transformComponent != nullptr) {
 			glm::mat4 l_ModelMatrix = glm::mat4(1.0);
 			glm::mat4 l_TransMatrix = glm::translate(glm::mat4(1.0f), aabbBoxComponent->GetPosition());
@@ -226,6 +235,7 @@ void RenderEngine::RenderSceneObjects(std::shared_ptr<ShaderProgram> p_ShaderPro
 			l_ModelMatrix = l_TransMatrix * l_ScaleMatrix;
 			RenderPhysicsDebug(l_ModelMatrix);
 		}
+#endif
 	}
 }
 
